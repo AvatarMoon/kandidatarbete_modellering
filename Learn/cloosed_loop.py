75931233eda5ed9e09e6841c7959d5358b4355d3
Mauritz <78498436+AvatarMoon@users.noreply.github.com>
2021-02-17 16:40:03 +0100

    Update cloosed_loop.py

--- Learn/cloosed_loop.py
+++ Learn/cloosed_loop.py
@@ -6,4 +6,7 @@ def ode_closed_loop(t,x,b)
     # Moste of the regular parameters
     b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b18, b19 b21, b22, b23, b25, b27 = b 
     S, H, L, G, C, I, W, E, M, A, Y, Q = X
+    
+    #Stomach glucose
+    dS = b9*H-b8*S
 
