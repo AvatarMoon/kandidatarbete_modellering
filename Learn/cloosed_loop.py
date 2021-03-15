29d5371077b17d2d51a86614611bf5adb681856e
BellaBerlanga <78801182+BellaBerlanga@users.noreply.github.com>
2021-03-15 15:56:57 +0100

    Fungerande cloosed loop

--- Learn/cloosed_loop.py
+++ Learn/cloosed_loop.py
@@ -24,11 +24,6 @@ def closed_loop(t,x):
     b10 = 0.022
     b11 = 0.02
     b12 = 28.66
-    b13 = 0.0000095
-    b14 = 0.0278
-    # b17 = 0.02 # random siffra
-    # b18 = 0.35
-    # b19 = 0.004
     b21 = 0.00876
     b22 = 0.0021
     b23 = 0.08
@@ -44,9 +39,15 @@ def closed_loop(t,x):
     e = 1
     Ge = 5
     c3 = 0.0554
+    
+    """ 
     k8 = 0.5275
-    # GLUT4 = 50.8472
-    GtA = 135
+    b13 = 0.0000095
+    b14 = 0.0278
+    # b17 = 0.02 # random siffra
+    # b18 = 0.35
+    # b19 = 0.004
+    GLUT4 = 50.8472GtA = 135
     GLUT1 = 0.0283
     KmG4 = 146.851
     KmG1 = 1.082 
@@ -59,12 +60,12 @@ def closed_loop(t,x):
     # Ib =
     q1 = 0.0031
     q2 = 0.4054
-    # Gb =
+    Gb = """
 
      
     
-    """ b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b17, b18, b19, b21, b22, b23, b25, b27 = b """
-    S, L, G, C, I, W, E, M, A, Y, Q, INSA, GtA = x 
+    """ b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b21, b22, b23, b25, b27 = b """
+    S, L, G, C, I, W, E, M, Q = x 
 
 
     # Stomach glucose [1]
@@ -98,22 +99,22 @@ def closed_loop(t,x):
     # dynamics of glucose mass in muscle tissue [8]
     dM = 0.1*(v/f)*b3*G*I*e - b27*M
 
-    # Adipose tissue glucose mass (A) [9] RÄTTA DENNA!!!
-    dA = k8*(GtA)/(KmG4 + GtA) + GLUT1*(GtA)/(KmG1 + GtA) - Kgluc*A
+    # Adipose tissue glucose mass (A) [9] Länk till cellulär nivå, ta bort om det går.
+    #dA = k8*(GtA)/(KmG4 + GtA) + GLUT1*(GtA)/(KmG1 + GtA) - Kgluc*A
 
     # Dynamics of plasma leptin (Y) [10]
-    dY = b13*A*Fat - b14*Y
+    #dY = b13*A*Fat - b14*Y
 
     # Dynamics of ghrelin concentration n plasma (Q) [11] RÄTTA DENNA!!!
     dQ = b12*math.exp(-S)*math.exp(-I) - b11*Q
 
     # Linking the whole body model with the cellular one [13] RÄTTA DENNA!!!
-    dINSA = -INSA + (I-2)
+    # dINSA = -INSA + (I-2)
 
     # Linking the whole body model with the cellular one [14]
-    dGtA = - q1*GtA + q2*(G - 2)
+    #dGtA = - q1*GtA + q2*(G - 2)
 
-    return [dS, dL, dG, dI, dW, dE, dC, dM, dA, dY, dQ, dINSA, dGtA]
+    return [dS, dL, dG, dI, dW, dE, dC, dM, dQ]
 
     # Ranges 
     """ range_G = [4.5, 11] # mM
@@ -127,21 +128,21 @@ def closed_loop(t,x):
     range_Q = [8, 1146]
     # range S, L = none """
  # time span
-t_vec = np.linspace(0.1, 20, num=50)
+t_vec = np.linspace(0.1, 120, num=20)
 time_span = [t_vec[0], t_vec[-1]] 
 # initial conditions
-x0 = [10, 20, 15, 10, 20, 15, 10, 20, 15, 10, 20, 15, 10]
+x0 = [10, 20, 15, 10, 20, 15, 10, 20, 15]
 # solve ODE
 sol = integrate.solve_ivp(closed_loop, time_span, x0, method="LSODA", t_eval=t_vec)
 
 # plot model
-# ymodel = sol.y[input("number between 0-13: ")]
-ymodel = sol.y[1]
+# ymodel = sol.y[input("number between 0-8: ")]
+ymodel = sol.y[0]
 plt.plot(t_vec, ymodel)
 plt.title("Simulated model")
 plt.show()
 """def ymodel():
-    R = range(13)
+    R = range(9)
     for i in R:
         model = sol.y[i]
         
