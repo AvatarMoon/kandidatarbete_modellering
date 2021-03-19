a21a09d20b45d8346b6893c22d798a8621b0a960
hannaavall <78952815+hannaavall@users.noreply.github.com>
2021-03-19 13:24:16 +0100

    lagt in datapunkterna manuellt

--- Learn/Sebbeskod_rev.py
+++ Learn/Sebbeskod_rev.py
@@ -28,6 +28,7 @@ def model(t, x, k):
     data is simulated using an additivate Gaussian noise of strength sigma (sigma
     is provided by the user). Note, for model1 x2 is treated as the output. 
 """
+"""
 def simulate_model1(t_vec, sigma):
      
     # Model parameters  
@@ -43,6 +44,7 @@ def simulate_model1(t_vec, sigma):
     # Extract x2 and add measurment noise 
     x1_obs = sol.y[0] + np.random.normal(loc=0.0, scale=sigma, size=n_data_points)
     return x1_obs 
+"""
 
 def simulate_model2(t_vec, sigma): #vårt andra set av datapunkter
     # Model parameters  
@@ -56,24 +58,36 @@ def simulate_model2(t_vec, sigma): #vårt andra set av datapunkter
     sol = integrate.solve_ivp(model, time_span, x0, method="LSODA", args=(rates, ), t_eval=t_vec)
     
     # Extract x2 and add measurment noise 
-    global x2_obs
     x2_obs = sol.y[1] + np.random.normal(loc=0.0, scale=sigma, size=n_data_points)
     return x2_obs
-    #print(x2_obs)
 
-#print(x2_obs) #varför hittar den inte denna?
 
 # We simulate data at 50 data-points from 0.1 to 2.0
-t_vec = np.linspace(0.1, 2.0, num=50)
+#t_vec = np.linspace(0.1, 2.0, num=50
+t_vec = [0.1    ,    0.11877551, 0.1255102 ,0.1332653 ,0.145510204, 0.287755,
+ 0.33965306 ,0.34142857 ,0.36020408 ,0.44897959 ,0.4877551  ,0.52653061,
+ 0.56530612 ,0.60408163 ,0.64285714, 0.68163265 ,0.72040816 ,0.75918367,
+ 0.79795918, 0.83673469 ,0.8755102 , 0.91428571 ,0.95306122 ,0.99183673,
+ 1.03061224 ,1.06938776 ,1.10816327, 1.14693878 ,1.18571429, 1.2244898,
+ 1.26326531, 1.30204082, 1.34081633, 1.37959184, 1.41836735, 1.45714286,
+ 1.49591837, 1.53469388, 1.57346939, 1.6122449 , 1.65102041, 1.68979592,
+ 1.72857143, 1.76734694 ,1.80612245, 1.84489796 ,1.88367347, 1.92244898,
+ 1.96122449, 2.        ]
+
 # Set seed to reproduce
 np.random.seed(123)
-<<<<<<< HEAD
-y_obs1 = simulate_model1(t_vec, 0.5) # dvs x1_obs
-y_obs2 = simulate_model2(t_vec, 0.5) # dvs x2_obs
-=======
-y_obs1 = simulate_model1(t_vec, 0.5) #här sätter vi in riktiga datapunkter
+#y_obs1 = simulate_model1(t_vec, 0.5) #här sätter vi in riktiga datapunkter
+y_obs1 = [ 9.4571847,  11.58919113, 12.21289911, 12.19099155, 13.42944331, 15.23699683,
+ 13.80917474, 15.35052827, 16.683327,   16.05016987, 16.52977931, 17.16621412,   
+ 18.26565248, 17.47295193, 17.81301694, 18.03340702, 19.54542822, 19.70676058,  
+ 19.26685423, 19.0924606,  19.38823203, 19.87314219, 18.7563838,  19.89791749,   
+ 18.75934244, 19.13566425, 19.96918621, 18.85538271, 19.54758238, 19.22925793,   
+ 19.57024161, 18.33232577, 18.87551843, 19.43765375, 20.27482079, 19.74526218,  
+ 19.85206147, 20.21120251, 19.4419978,  20.03679809, 19.50410396, 19.05339902,   
+ 19.73104368, 20.22163912, 20.11140778, 19.94272682, 21.15058481, 20.16595113,   
+ 20.45339015, 21.08711211]
+
 y_obs2 = simulate_model2(t_vec, 0.5) 
->>>>>>> d3918faa84b0ed194c93507fe18ad7122dad699f
 
 print(y_obs2)
 
@@ -117,9 +131,7 @@ print(res.fun)
 
 
 # Plotting observed data at time-points 0.1, ..., 2.0 (we have 50 data-points)
-
-print (y_obs1)
-print(t_vec)
+print("y = {}, t = {}".format(y_obs1, t_vec))
 plot1 = plt.figure(1)
 plt.plot(t_vec, y_obs1)
 plt.title("Simulated data and model1")
