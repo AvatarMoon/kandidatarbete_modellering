35057b2f183980084e2973c24e552c7329e6d947
BellaBerlanga <78801182+BellaBerlanga@users.noreply.github.com>
2021-03-22 13:36:32 +0100

    Update Openloop_optimisering.py

--- Optimisering/Openloop_optimisering.py
+++ Optimisering/Openloop_optimisering.py
@@ -6,31 +6,35 @@ import math
 import pandas as pd 
 
 # get data
-Glucose = pd.read_csv()
-Insulin = pd.read_csv()
+"""Glucose = pd.read_csv()
+Insulin = pd.read_csv()"""
+
+c0 = 1.8854
+c1 = 198
+c2 = 94
+c3 = 0.0554
+b1 = 0.0059
+b2 = 0.1262
+b3 = 0.00005
+b4 = 0.4543
+b5 = 0.185
+b6= 0.8
+b7= 0.8
+b8= 0.8
+b9 = 0.8
+b10 = 0.022
+b21 = 0.00876
+b22 = 0.0021
+b23 = 0.08
+b25 = 0.00026
+b27 = 0.014
+b100 = 0.3
+Ge = 5
+f = 0.9
+v = 15
 
 def open_loop(t,x,b):
-    b1, b3, b5, b6, b7, b8, b9, b10, f, v = b
-
-    c0 = 1.8854
-    c1 = 198
-    c2 = 94
-    c3 = 0.0554
-    b1 = 0.0059
-    b2 = 0.1262
-    b3 = 0.00005
-    b4 = 0.4543
-    b5 = 0.185
-    b10 = 0.022
-    b21 = 0.00876
-    b22 = 0.0021
-    b23 = 0.08
-    b25 = 0.00026
-    b27 = 0.014
-    b100 = 0.3
-    Ge = 5
-    f = 0.9
-    v = 15
+    # b1, b3, b5, b6, b7, b8, b9, b10, f, v = b
     
     # Says that the concentrations can't be lower than zero
     x[x < 0] = 0.0
@@ -61,9 +65,7 @@ def simulate_model(t_vec, sigma):
      
     # Model parameters 
     x0 = [1, 0.8, 0.5, 0.1, 0.2, 0.5]   # initial
-    t_vec = [0.231756251, 0.17399052, 3.205728606, 3.06, 6,229764595, 10,80577321, 15,76784945, 21,44814628, 27,10148577, 36,36133236, 44,19243988, 51.32843312, 59.14413644, 70.19568344, 81.86532375, 92.49999473, 96.041034, 98.8638527, 106.679556, 113.7770388, 120.9014789, 128.7595437, 135.6018945, 146.2904802, 157.7049885, 173.0080932, 194.1262815,, 86.71186854, 84.17884126, 82.29471568, 80.7697004, 79.17921729, 79.10219631
-78.15291281
-]
+    t_vec = [0.231756251, 0.17399052, 3.205728606, 3.06, 6,229764595, 10,80577321, 15,76784945, 21,44814628, 27,10148577, 36,36133236, 44,19243988, 51.32843312, 59.14413644, 70.19568344, 81.86532375, 92.49999473, 96.041034, 98.8638527, 106.679556, 113.7770388, 120.9014789, 128.7595437, 135.6018945, 146.2904802, 157.7049885, 173.0080932, 194.1262815, 86.71186854, 84.17884126, 82.29471568, 80.7697004, 79.17921729, 79.10219631, 78.15291281]
     time_span = [t_vec[0], t_vec[-1]] # 0 är första och -1 är sista
     rates = [b1, b3, b5, b6, b7, b8, b9, b10, f, v] #parametrar
     n_data_points = len(t_vec)
@@ -99,11 +101,11 @@ def cost_function(b, y0_obs):
     
     # Step 1: Solve ODE-system 
     sol = integrate.solve_ivp(open_loop, time_span, x0, method="LSODA", args=(b, ), t_eval=t_vec)
-    class scipy.optimize.Bounds(lb, ub, keep_feasible=False) # vart ska denna in och hur sätter man olika bounds för olika konc.
-    class scipy.optimize.LinearConstraint(A, lb, ub, keep_feasible=False)[source]
+    """class scipy.optimize.Bounds(lb, ub, keep_feasible=False) # vart ska denna in och hur sätter man olika bounds för olika konc.
+    """
     
     # Step 2: Extract x0 (simulated y-vec) , denna delen varierar beroende på del av data, def fkn här.
-    global y_model
+    #global y_model
     
     #y_model = sol.y[0] # byt ut siffran i y[], nr 0-13
     y0_model = sol.y[0]
@@ -118,6 +120,7 @@ def cost_function(b, y0_obs):
 
 # Note, a numerical optmizer require a starting guess, here I use the start-guess (0.022, 0.022, 0.022)
 res = minimize(cost_function, [b1, b3, b5, b6, b7, b8, b9, b10, f, v], method='Powell', args = (y0_obs, )) #lägg in constraints här
+ """loop för olika rates/startgissningar""" 
 
 # Print some statistics 
 print("Optimal value found via Powells-method:")
