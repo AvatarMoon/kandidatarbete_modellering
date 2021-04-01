3a067189ad9bc3d6728969ae3027c7feb0d86bbc
Mauritz <78498436+AvatarMoon@users.noreply.github.com>
2021-04-01 16:34:46 +0200

    update

--- data.py
+++ data.py
@@ -1,15 +1,11 @@
 import numpy as np
 import os
 import csv
-import pandas as pd
 
 data_path = os.listdir('data_horses')
 
-conc = [] 
-
+conc = []
 for x in data_path:
-    # data = pd.read_csv('data_horses/' + x, sep = ';')
-    # conc = data.to_string()
     values_list = []
     with open('data_horses/' + x) as file:
         for line in file.readlines()[1:]:
@@ -18,4 +14,8 @@ for x in data_path:
             values_list.append(value)
     conc.append(values_list)
 
-print(conc)
+g_data = conc[0]
+i_data = conc[1]
+
+print(g_data)
+print(i_data)
