29c0ceb159b6e1fb80586f780c4174ba435dcb87
Linneamax <78621149+Linneamax@users.noreply.github.com>
2021-04-13 11:11:27 +0200

    plot change

--- optimering/plot_raw_data.py
+++ optimering/plot_raw_data.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
-import numpy 
+import numpy as np
 import pandas as pd
 import os
+import scipy.integrate as integrate
 
 # Colour-blind friendly palette (use nice colors)
 cb_palette1 = ["#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"]
@@ -19,25 +20,84 @@ data_I = data_I.sort_values(by = ['tid'])
 tG_vec = data_G['tid'].values
 tI_vec = data_I['tid'].values
 
-# Constrains glukos
+time_span_G = [tG_vec[0], tG_vec[-1]]
+time_span_I = [tI_vec[0], tI_vec[-1]]
+
+# Modellen
+def open_loop(t,x,b): 
+    c0, c1, c2, c3, b1, b2, b3, b4, b5, b10, b21, b22, b23, b25, b27, b100, f, v = b
+
+    Ge = 5
+     
+    # Says that the concentrations can't be lower than zero 
+    x[x < 0] = 0.0 
+
+    # Concentrations in the model as input 
+    G, I, E, C, M, H = x 
+
+    # Glucose plasma [1]  G0 closed loop = 5mM 
+    dG = f*b10*H/v + f*b5*C/v - b1*G - b3*I*G 
+
+    # Insulin plasma [2]  60 pM 
+    dI = b4*G - b2*I    
+
+    # Glucacon plasma [3] E0 closed loop = 34 pM 
+    dE = c0 + (c1/(c2 + I))*(Ge - G)*np.heaviside(Ge-G,1) - c3*E  
+
+    # GLucose liver [4] C0 closed loop = 3 mmol 
+    dC = b23 - b25*I - b22*G + b21*E - b5*C 
+
+    # Glucose musle [5] M0 = 2.5 mmol 
+    dM = 0.1*(v/f)*b3*G*I - b27*M 
+
+    # Glucos intake [6]  H0 = 200 mmol 
+    dH = - b100*H*G 
+
+    return [dG, dI, dE, dC, dM, dH] 
+
+# Inital values for concentrations and optimal values for parameters 1 start guess
+x0 = [5, 60, 34, 3, 2.5, 200]   
+b = [1.65196917, 1.43044967, 2.19742316e+04, 5.59792273e+01, 2.84072466e+02, 6.46691412, 1.31869684e+01, 3.09662271e+03, 4.20835897e-02, 8.93446278e-02, 3.91588623, 1.04056980, 3.81839534, 2.73503274, 6.48068508, 1.15970185e+01, 3.11515367, 1.58857288e+01]
+
+sol_qual_G = integrate.solve_ivp(open_loop, time_span_G, x0, method="LSODA", args=(b, ), t_eval = tG_vec)
+sol_qual_I = integrate.solve_ivp(open_loop, time_span_I, x0, method="LSODA", args=(b, ), t_eval = tI_vec)
+
+G_model = sol_qual_G.y[0]
+I_model = sol_qual_I.y[1]
+E_model = sol_qual_G.y[2]
+C_model = sol_qual_G.y[3]
+M_model = sol_qual_G.y[4]
+
+# Constrains glukos (G)
 xG_coordinates = [tG_vec[0],tG_vec[-1]]
 yG1_coordinates = [4.5,4.5] #mM (human)
 yG2_coordinates = [11,11]   #mM (human)
 
-# Constrains insulin
+# Constrains insulin (I)
 xI_coordinates = [tI_vec[0],tI_vec[-1]]
 yI1_coordinates = [38,38]   #pM (human)
 yI2_coordinates = [400,400] #pM (human)
+
+# Constrains glucagon (E)
+xE_coordinates = [tG_vec[0],tG_vec[-1]] 
+yE1_coordinates = [28.68,28.68]  #pM (human)
+yE2_coordinates = [47.04,47.04]  #pM (human)
+ 
+ # Constrains Glukos i lever (C)
+xC_coordinates = [tG_vec[0],tG_vec[-1]] 
+yC1_coordinates = [0,0]  # mmol (human)
+yC2_coordinates = [8,8]  # mmol (human)
+
+ # Constrains Glukos i muskel (M)
+xM_coordinates = [tG_vec[0],tG_vec[-1]] 
+yM1_coordinates = [2,2]    # mmol (human)
+yM2_coordinates = [13,13]  # mmol (human)
 
 # plotta glukos
 lw = 2.0
 plot1 = plt.figure(1)
 line1, = plt.plot(xG_coordinates, yG1_coordinates, linestyle=":", linewidth=lw, color=cb_palette2[1])
-
-plot1 = plt.figure(1)
 line2, = plt.plot(xG_coordinates, yG2_coordinates, linestyle=":", linewidth=lw, color=cb_palette2[3])
-
-plot1 = plt.figure(1)
 line3, = plt.plot(data_G['tid'].values, data_G['konc'].values, label = 'Glukos', linestyle="-", linewidth=lw, color=cb_palette2[2])
 plt.legend((line1, line2, line3), ("Lägsta gräns", "Högsta gräns", "Data"))
 plt.xlabel("time", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
@@ -57,16 +117,11 @@ plt.savefig(path_fig)
 lw = 2.0
 plot1 = plt.figure(2)
 line1, = plt.plot(xI_coordinates, yI1_coordinates, linestyle=":", linewidth=lw, color=cb_palette2[1])
-
-plot1 = plt.figure(2)
 line2, = plt.plot(xI_coordinates, yI2_coordinates, linestyle=":", linewidth=lw, color=cb_palette2[3])
-
-plot1 = plt.figure(2)
 line3, = plt.plot(data_I['tid'].values, data_I['conc'].values, label = 'Insulin', linestyle="-", linewidth=lw, color=cb_palette2[4])
 plt.legend((line1, line2, line3), ("Lägsta gräns", "Högsta gräns", "Data"))
 plt.xlabel("time", fontsize=12), plt.ylabel("Insulin koncentration", fontsize=12)
 plt.title("Insulin in plasma")
-plt.show()
 
 # Sparar figur i plot constrains, insulin
 # Write the result to file
@@ -75,5 +130,66 @@ path_result_dir = "optimering/Bilder/plot_constrains"
 if not os.path.isdir(path_result_dir):
     os.mkdir(path_result_dir)  # Create a new directory if not existing
 path_fig = path_result_dir + "/plot_insulin.jpg"
+print("path_fig = {}".format(path_fig))
+plt.savefig(path_fig)
+
+# plotta glucagon
+lw = 2.0
+plot1 = plt.figure(3)
+line1, = plt.plot(xE_coordinates, yE1_coordinates, linestyle=":", linewidth=lw, color=cb_palette2[1])
+line2, = plt.plot(xE_coordinates, yE2_coordinates, linestyle=":", linewidth=lw, color=cb_palette2[3])
+line3, = plt.plot(data_G['tid'].values, E_model, label = 'Glucagon', linestyle="-", linewidth=lw, color=cb_palette2[4]) # Lägga till modellen
+plt.legend((line1, line2, line3), ("Lägsta gräns", "Högsta gräns", "Modell"))
+plt.xlabel("time", fontsize=12), plt.ylabel("Glucagon koncentration", fontsize=12)
+plt.title("Glucagon i plasman")
+
+# Sparar figur i plot constrains, insulin
+# Write the result to file
+path_result_dir = "optimering/Bilder/plot_constrains"
+# Check if directory exists
+if not os.path.isdir(path_result_dir):
+    os.mkdir(path_result_dir)  # Create a new directory if not existing
+path_fig = path_result_dir + "/plot_glucagon.jpg"
+print("path_fig = {}".format(path_fig))
+plt.savefig(path_fig)
+
+# plotta Glukos i lever 
+lw = 2.0
+plot1 = plt.figure(4)
+line1, = plt.plot(xC_coordinates, yC1_coordinates, linestyle=":", linewidth=lw, color=cb_palette2[1])
+line2, = plt.plot(xC_coordinates, yC2_coordinates, linestyle=":", linewidth=lw, color=cb_palette2[3])
+line3, = plt.plot(data_G['tid'].values, C_model, label = 'Glukos i levern', linestyle="-", linewidth=lw, color=cb_palette2[6]) # Lägga till modellen
+plt.legend((line1, line2, line3), ("Lägsta gräns", "Högsta gräns", "Modell"))
+plt.xlabel("time", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
+plt.title("Glukos i levern")
+
+# Sparar figur i plot constrains, insulin
+# Write the result to file
+path_result_dir = "optimering/Bilder/plot_constrains"
+# Check if directory exists
+if not os.path.isdir(path_result_dir):
+    os.mkdir(path_result_dir)  # Create a new directory if not existing
+path_fig = path_result_dir + "/plot_glukoslevern.jpg"
+print("path_fig = {}".format(path_fig))
+plt.savefig(path_fig)
+
+# plotta Glukos i muskeln 
+lw = 2.0
+plot1 = plt.figure(5)
+line1, = plt.plot(xM_coordinates, yM1_coordinates, linestyle=":", linewidth=lw, color=cb_palette2[1])
+line2, = plt.plot(xM_coordinates, yM2_coordinates, linestyle=":", linewidth=lw, color=cb_palette2[3])
+line3, = plt.plot(data_G['tid'].values, M_model, label = 'Glukos i muskeln', linestyle="-", linewidth=lw, color=cb_palette2[7]) # Lägga till modellen
+plt.legend((line1, line2, line3), ("Lägsta gräns", "Högsta gräns", "Modell"))
+plt.xlabel("time", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
+plt.title("Glukos i muskeln")
+plt.show()
+
+# Sparar figur i plot constrains, insulin
+# Write the result to file
+path_result_dir = "optimering/Bilder/plot_constrains"
+# Check if directory exists
+if not os.path.isdir(path_result_dir):
+    os.mkdir(path_result_dir)  # Create a new directory if not existing
+path_fig = path_result_dir + "/plot_glukosmuskeln.jpg"
 print("path_fig = {}".format(path_fig))
 plt.savefig(path_fig)
