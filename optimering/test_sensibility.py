50adfa39100e92e603a2e93d5678108d42130096
Mauritz <78498436+AvatarMoon@users.noreply.github.com>
2021-04-20 09:30:16 +0200

    Work sensibiliy

--- optimering/test_sensibility.py
+++ optimering/test_sensibility.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import scipy.integrate as integrate 
 
+
 def open_loop(t,x,b): 
     c0, c1, c2, c3, b1, b2, b3, b4, b5, b10, b21, b22, b23, b25, b27, b100, f, v = b
 
@@ -34,6 +35,8 @@ def open_loop(t,x,b):
 
 x0 = [5, 60, 34, 3, 2.5, 200]
 t_vec = np.linspace(0, 240, num=20)
+#start_values taken from openloop_optimering
+# start_values = np.array(res.x)
 start_values = np.array([1.885, 198, 94, 0.0554, 0.0059, 0.1262, 0.00005, 0.4543, 0.185, 0.022, 0.00876, 0.0021, 0.08, 0.00026, 0.014, 0.3, 0.9, 15])
 
 def sensitivity(b,t,x):
@@ -41,7 +44,7 @@ def sensitivity(b,t,x):
     n_par = len(b)
     t_len = len(t)
     S = np.zeros([len(x), n_par, t_len])
-    time_span = t
+    time_span = [t[0], t[-1]]
 
     for n in range(len(b)):
         b1 = b.copy() 
@@ -49,11 +52,16 @@ def sensitivity(b,t,x):
         b1[n] += h 
         b2[n] -= h
 
-        Sol_high = integrate.solve_ivp(open_loop, time_span, x, method='LSODA', args=(b1, ), t_eval = time_span)
-        Sol_low = integrate.solve_ivp(open_loop, time_span, x, method='LSODA', args=(b2, ), t_eval= time_span)
+        Sol_high = integrate.solve_ivp(open_loop, time_span, x, method='LSODA', args=(b1, ), t_eval = t)
+        Sol_low = integrate.solve_ivp(open_loop, time_span, x, method='LSODA', args=(b2, ), t_eval= t)
         
         for m in range(len(x)):
             S[m, n, 0:t_len] = (Sol_high.y[m]-Sol_low.y[m]) / (2 * h) 
     return S
 
 S = sensitivity(start_values, t_vec, x0)
+
+fisher = 2*S*np.transpose(S)
+
+print(S)
+print(fisher)
