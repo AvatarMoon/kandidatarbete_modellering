9f41d3498785e39c5a782201533ebe8442181d9e
Mauritz <78498436+AvatarMoon@users.noreply.github.com>
2021-04-26 11:37:00 +0200

    Work in progress

--- Modeller/week16_open_loop.py
+++ Modeller/week16_open_loop.py
@@ -15,7 +15,7 @@ def open_loop(t,x):
     x[x < 0] = 0.0
 
     # Concentrations in the model as input
-    G, I, C, M = x
+    G, I, C, M, H = x
 
     # Parametrar
     k1 = 0.1
@@ -23,10 +23,8 @@ def open_loop(t,x):
     k3 = 0.1
     k4 = 0.1
     k5 = 0.1 
-    k6 = 0.1
 
-    # Startvärden
-    H= 600 # Intag glukos 
+    
     L= 500 # Startvärde glukos i levern
 
     # Glucose plasma [1]
@@ -41,21 +39,24 @@ def open_loop(t,x):
     # Glucose musle [5]
     dM = k2*G - k5*M
 
-    return [dG, dI, dC, dM]
+    # Glucose intake 
+    dH = -k1 * H
+
+    return [dG, dI, dC, dM, dH]
 
 # Time span
 t_vec = np.linspace(0, 240, num=20)
 time_span = [t_vec[0], t_vec[-1]]
 
 # Initial values to model
-x0 = [60, 5, 34, 3]  # G, I, C, M
+x0 = [60, 5, 34, 3, 70]  # G, I, C, M
 
 # Solve ODE
 sol = integrate.solve_ivp(open_loop, time_span, x0, method="LSODA", t_eval=t_vec)
 
 # Plot model
 
-ymodel = sol.y[1]
+ymodel = sol.y[0]
 plt.plot(t_vec, ymodel)
 plt.title('Open loop-model')
 plt.show()
