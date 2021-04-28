c54f4a95d304db32f0f0981335d8faab6b41ea1e
Mauritz <78498436+AvatarMoon@users.noreply.github.com>
2021-04-28 17:23:45 +0200

    more plots

--- test.py
+++ test.py
@@ -8,6 +8,43 @@ import pandas as pd
 import os
 import lhsmdu
 
+
+# Colour-blind friendly palette (use nice colors)
+cb_palette1 = ["#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"]
+
+## ~~~ Plot random vs hypercube
+
+l = lhsmdu.sample(2,20) # Latin Hypercube Sampling of two variables, and 10 samples each.
+k = lhsmdu.createRandomStandardUniformMatrix(2,20) # Monte Carlo Sampling
+
+
+fig1 = plt.figure(1)
+plt.scatter([k[0]], [k[1]], color=cb_palette1[0])
+plt.title("Random")
+
+# Write the result to file
+path_result_dir = "Modeller/"
+# Check if directory exists
+if not os.path.isdir(path_result_dir):
+    os.mkdir(path_result_dir)  # Create a new directory if not existing
+path_fig = path_result_dir + "/random.pdf"
+print("path_fig = {}".format(path_fig))
+plt.savefig(path_fig)
+
+fig2 = plt.figure(2)
+plt.scatter([l[0]], [l[1]], color=cb_palette1[1])
+plt.title("LHS")
+
+# Write the result to file
+path_result_dir = "Modeller/"
+# Check if directory exists
+if not os.path.isdir(path_result_dir):
+    os.mkdir(path_result_dir)  # Create a new directory if not existing
+path_fig = path_result_dir + "/LHS.pdf"
+print("path_fig = {}".format(path_fig))
+plt.savefig(path_fig)
+
+
 def model1(t, x, k):
     k1, k2 = k
     
@@ -16,8 +53,6 @@ def model1(t, x, k):
     
     return [dx1, dx2]
 
-# Colour-blind friendly palette (use nice colors)
-cb_palette1 = ["#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"]
 
 # get data 
 data_G = pd.read_csv ("data_horses/glukos_FF_training.csv", sep=';')
