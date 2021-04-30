51dac6ed8ed0df0a56e56921a6866827287e99de
Linneamax <78621149+Linneamax@users.noreply.github.com>
2021-04-30 13:08:39 +0200

    new model etc

--- optimering/week16_open_loop_optimering.py
+++ optimering/week16_open_loop_optimering.py
@@ -26,56 +26,65 @@ cI_vec = data_I['conc'].values
 
 
 def open_loop(t,x,b): 
-    k1, k2, k3, k4, k5 = b
+    k1, k2, k3, k4, k5, k6, k7, k8 = b
      
     # Says that the concentrations can't be lower than zero 
     x[x < 0] = 0.0 
 
     # Concentrations in the model as input 
-    G, I, C, M, H = x 
+    G, I, C, M, H, E, F = x 
 
-    L= 500 # Startvärde glukos i levern
+    #L= 5000 # Startvärde glukos i levern
 
     # Glucose plasma [1]
-    dG = k4*C*I + k1*H - k2*M
+    dG = k4*C + k1*H - k2*G*I
 
     # Insulin plasma [2]
-    dI = -k4*I*C + k3*G
+    dI = k3*G - k2*G*I
 
     # GLucose liver [3]
-    dC = -k4*C*I + L
+    dC = -k4*C + k6*E + k7*F
 
     # Glucose musle [4]
-    dM = k2*G - k5*M
+    dM = k2*G*I - k5*M
 
     # Glucose intake [5]
     dH = -k1*H
 
-    return [dG, dI, dC, dM, dH]
+    # Glucagon [6]
+    dE = k8 - k2*G*E 
 
- 
+    # Fettreserv [7]
+    dF = -k7*F
+
+    return [dG, dI, dC, dM, dH, dE, dF]
+
+
 
 def cost_function(b, yG_vec, yI_vec): 
 
     # Start concentration, timespan   
-    x0 = [60, 5, 34, 3, 70]  # G, I, C, M, H 
+    x0 = [30, 100, 34, 60, 70, 50, 400]  # G, I, C, M, H, E, F 
     time_span_G = [tG_vec[0], tG_vec[-1]] 
     time_span_I = [tI_vec[0], tI_vec[-1]] 
     
 
     # Step 1: Solve ODE-system at points tG_vec
-    sol_G = integrate.solve_ivp(open_loop, time_span_G, x0, method="LSODA", args=(b, ), t_eval=tG_vec) 
-    sol_I = integrate.solve_ivp(open_loop, time_span_I, x0, method="LSODA", args=(b, ), t_eval=tI_vec) 
+    sol_G = integrate.solve_ivp(open_loop, time_span_G, x0, method="Radau", args=(b, ), t_eval=tG_vec) 
+    sol_I = integrate.solve_ivp(open_loop, time_span_I, x0, method="Radau", args=(b, ), t_eval=tI_vec) 
 
     
     # step 2: Solve ODE-system qualitative
-    sol_qual = integrate.solve_ivp(open_loop, time_span_G, x0, method="LSODA", args=(b, ))
+    sol_qual = integrate.solve_ivp(open_loop, time_span_G, x0, method="Radau", args=(b, ))
 
     G_model = sol_qual.y[0]
     I_model = sol_qual.y[1]
     C_model = sol_qual.y[2]
     M_model = sol_qual.y[3]
     H_model = sol_qual.y[4]
+    E_model = sol_qual.y[5]
+    F_model = sol_qual.y[6]
+
 
 
     # Step 3: Extract G and I model concentrations at t-points tG_vec and tI_vec
@@ -85,11 +94,15 @@ def cost_function(b, yG_vec, yI_vec):
     # Step 4 : Build bounds for the concentrations and punnish the cost-func. if they go cross the bounds
     squared_sum = 0.0
 
-    range_G = [0, 50] # mM 
-    range_I = [0, 5000] #pM 
-    range_C = [0, 100] # mmol 
-    range_M = [0, 140] # mmol
+    range_G = [0, 500] # mM 
+    range_I = [0, 4000] #pM 
+    range_C = [0, 1000] # mmol 
+    range_M = [0, 1000] # mmol
     range_H = [0, 500] # mmol
+    range_E = [0, 500] # mmol
+    range_F = [0, 500] # mmol
+
+
 
      
 
@@ -113,6 +126,14 @@ def cost_function(b, yG_vec, yI_vec):
        squared_sum += 100
     if any(H_model) < np.min(range_H):
         squared_sum += 100
+    if any(E_model) > np.max(range_E):
+       squared_sum += 100
+    if any(E_model) < np.min(range_E):
+        squared_sum += 100
+    if any(F_model) > np.max(range_F):
+       squared_sum += 100
+    if any(F_model) < np.min(range_F):
+        squared_sum += 100
     
 
     # Step 5: Calculate cost-function  
@@ -123,36 +144,42 @@ def cost_function(b, yG_vec, yI_vec):
 ## Hypercube set up
 randSeed = 2 # random number of choice
 lhsmdu.setRandomSeed(randSeed) # Latin Hypercube Sampling with multi-dimensional uniformity
-start = np.array(lhsmdu.sample(5, 1)) # Latin Hypercube Sampling with multi-dimensional uniformity (parameters, samples)
+start = np.array(lhsmdu.sample(8, 1)) # Latin Hypercube Sampling with multi-dimensional uniformity (parameters, samples)
 
 para, samples = start.shape
 
 ## intervals for the parameters
-para_int = [0, 500]
+para_int = [0, 500, 100, 50]
 
 minimum = (np.inf, None)
 
 # Bounds for the model
-bound_low = np.array([0, 0, 0, 0, 0])
+bound_low = np.array([0, 0, 0, 0, 0, 0, 0, 0])
 bound_upp = np.repeat(np.inf, para)
 bounds = Bounds(bound_low, bound_upp)
 
 
 for n in range(samples):
-    k1 = start[0,n] * para_int[1]
+    k1 = start[0,n] * para_int[2]
     k2 = start[1,n] * para_int[1]
     k3 = start[2,n] * para_int[1]
     k4 = start[3,n] * para_int[1]
     k5 = start[4,n] * para_int[1]
+    k6 = start[5,n] * para_int[1]
+    k7 = start[6,n] * para_int[3]
+    k8 = start[7,n] * para_int[1]
+
+
+
     
-    res = minimize(cost_function, [k1, k2, k3, k4, k5], method='Powell', args = (cG_vec, cI_vec), bounds=bounds) #lägg in constraints här 
+    res = minimize(cost_function, [k1, k2, k3, k4, k5, k6, k7, k8], method='Powell', args = (cG_vec, cI_vec), bounds=bounds) #lägg in constraints här 
 
     if res.fun < minimum[0]:
         minimum = (res.fun, res.x)
 
 # Hämta modellen
 # Start concentration, timespan   
-x0 = [60, 5, 34, 3, 70]  # G, I, C, M, H 
+x0 = [30, 100, 34, 60, 70, 50, 400]  # G, I, C, M, H, E, F 
 time_span_G = [tG_vec[0], tG_vec[-1]] 
 time_span_I = [tI_vec[0], tI_vec[-1]] 
 
@@ -165,6 +192,10 @@ I_model = sol_qual.y[1]
 C_model = sol_qual.y[2]
 M_model = sol_qual.y[3]
 H_model = sol_qual.y[4]
+E_model = sol_qual.y[5]
+F_model = sol_qual.y[6]
+
+
 
 
 # Print some statistics  
@@ -185,20 +216,27 @@ yG2_coordinates = [50,50]   #mM (human)
 
 # Constrains insulin (I)
 yI1_coordinates = [0,0]   #pM (human)
-yI2_coordinates = [5000,5000] #pM (human)
+yI2_coordinates = [4000,4000] #pM (human)
  
  # Constrains glukos i lever (C) 
 yC1_coordinates = [0,0]  # mmol (human)
-yC2_coordinates = [100,100]  # mmol (human)
+yC2_coordinates = [1000,1000]  # mmol (human)
 
  # Constrains glukos i muskel (M) 
 yM1_coordinates = [0,0]    # mmol (human)
-yM2_coordinates = [140,140]  # mmol (human)
+yM2_coordinates = [1000,1000]  # mmol (human)
 
  # Constrains glucose intake (H)
 yH1_coordinates = [0,0]    
-yH2_coordinates = [500,500]  
+yH2_coordinates = [200,200]  
 
+ # Constrains glucagon plasma (E)
+yE1_coordinates = [0,0]    
+yE2_coordinates = [200,200]  
+
+ # Constrains Fettreserver (F)
+yF1_coordinates = [0,0]    
+yF2_coordinates = [500,500] 
 
 # plotta glukos
 lw = 2.0
@@ -208,8 +246,8 @@ line2, = plt.plot(xT_coordinates, yG2_coordinates, linestyle=":", linewidth=lw, 
 line3, = plt.plot(data_G['time'].values, data_G['conc'].values, label = 'Glukos', linestyle="-", linewidth=lw, color=cb_palette1[7])
 line4, = plt.plot(time_span, G_model, label = 'Glukos', linestyle="-", linewidth=lw, color=cb_palette1[5])
 plt.legend((line4, line3, line2, line1), ("Modell", "Data", "Högsta gräns","Lägsta gräns"))
-plt.xlabel("time", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
-plt.title("Glucose in plasma")
+plt.xlabel("tid (min)", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
+plt.title("Glukos i plasma")
 
 # Sparar figur i plot constrains, glukos
 # Write the result to file
@@ -229,8 +267,8 @@ line2, = plt.plot(xT_coordinates, yI2_coordinates, linestyle=":", linewidth=lw, 
 line3, = plt.plot(data_I['time'].values, data_I['conc'].values, label = 'Insulin', linestyle="-", linewidth=lw, color=cb_palette1[7])
 line4, = plt.plot(time_span, I_model, label = 'Insulin', linestyle="-", linewidth=lw, color=cb_palette1[5])
 plt.legend((line4, line3, line2, line1), ("Modell", "Data", "Högsta gräns","Lägsta gräns"))
-plt.xlabel("time", fontsize=12), plt.ylabel("Insulin koncentration", fontsize=12)
-plt.title("Insulin in plasma")
+plt.xlabel("tid", fontsize=12), plt.ylabel("Insulin koncentration", fontsize=12)
+plt.title("Insulin i plasma")
 
 # Sparar figur i plot constrains, insulin
 # Write the result to file
@@ -249,7 +287,7 @@ line1, = plt.plot(xT_coordinates, yC1_coordinates, linestyle=":", linewidth=lw, 
 line2, = plt.plot(xT_coordinates, yC2_coordinates, linestyle=":", linewidth=lw, color=cb_palette1[3])
 line3, = plt.plot(time_span, C_model, label = 'Glukos i levern', linestyle="-", linewidth=lw, color=cb_palette1[5]) # Lägga till modellen
 plt.legend((line3, line2, line1), ("Modell", "Högsta gräns", "Lägsta gräns"))
-plt.xlabel("time", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
+plt.xlabel("tid", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
 plt.title("Glukos i levern")
 
 # Sparar figur i plot constrains, glukos i levern
@@ -269,7 +307,7 @@ line1, = plt.plot(xT_coordinates, yM1_coordinates, linestyle=":", linewidth=lw, 
 line2, = plt.plot(xT_coordinates, yM2_coordinates, linestyle=":", linewidth=lw, color=cb_palette1[3])
 line3, = plt.plot(time_span, M_model, label = 'Glukos i muskeln', linestyle="-", linewidth=lw, color=cb_palette1[5]) # Lägga till modellen
 plt.legend((line3, line2, line1), ("Modell", "Högsta gräns", "Lägsta gräns"))
-plt.xlabel("time", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
+plt.xlabel("tid", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
 plt.title("Glukos i muskeln")
 
 
@@ -291,7 +329,7 @@ line1, = plt.plot(xT_coordinates, yH1_coordinates, linestyle=":", linewidth=lw, 
 line2, = plt.plot(xT_coordinates, yH2_coordinates, linestyle=":", linewidth=lw, color=cb_palette1[3])
 line3, = plt.plot(time_span, H_model, label = 'Glukos intag', linestyle="-", linewidth=lw, color=cb_palette1[5]) # Lägga till modellen
 plt.legend((line3, line2, line1), ("Modell", "Högsta gräns", "Lägsta gräns"))
-plt.xlabel("time", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
+plt.xlabel("tid", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
 plt.title("Glukos intag")
 
 
@@ -302,5 +340,47 @@ path_result_dir = "optimering/Bilder/plot_week16_open_loop_model"
 if not os.path.isdir(path_result_dir):
     os.mkdir(path_result_dir)  # Create a new directory if not existing
 path_fig = path_result_dir + "/plot_glukos_intag.jpg"
+print("path_fig = {}".format(path_fig))
+plt.savefig(path_fig)
+
+# plotta Glucagon
+lw = 2.0
+plot1 = plt.figure(6)
+line1, = plt.plot(xT_coordinates, yE1_coordinates, linestyle=":", linewidth=lw, color=cb_palette1[1])
+line2, = plt.plot(xT_coordinates, yE2_coordinates, linestyle=":", linewidth=lw, color=cb_palette1[3])
+line3, = plt.plot(time_span, E_model, label = 'Glucagon', linestyle="-", linewidth=lw, color=cb_palette1[5]) # Lägga till modellen
+plt.legend((line3, line2, line1), ("Modell", "Högsta gräns", "Lägsta gräns"))
+plt.xlabel("tid", fontsize=12), plt.ylabel("Glukagon koncentration", fontsize=12)
+plt.title("Glucagon")
+
+
+# Sparar figur i plot constrains, glukos i muskeln
+# Write the result to file
+path_result_dir = "optimering/Bilder/plot_week16_open_loop_model"
+# Check if directory exists
+if not os.path.isdir(path_result_dir):
+    os.mkdir(path_result_dir)  # Create a new directory if not existing
+path_fig = path_result_dir + "/plot_glukagon_i_plasma.jpg"
+print("path_fig = {}".format(path_fig))
+plt.savefig(path_fig)
+
+# plotta Fettreserver
+lw = 2.0
+plot1 = plt.figure(7)
+line1, = plt.plot(xT_coordinates, yF1_coordinates, linestyle=":", linewidth=lw, color=cb_palette1[1])
+line2, = plt.plot(xT_coordinates, yF2_coordinates, linestyle=":", linewidth=lw, color=cb_palette1[3])
+line3, = plt.plot(time_span, F_model, label = 'Fettreserver', linestyle="-", linewidth=lw, color=cb_palette1[5]) # Lägga till modellen
+plt.legend((line3, line2, line1), ("Modell", "Högsta gräns", "Lägsta gräns"))
+plt.xlabel("tid", fontsize=12), plt.ylabel("Glukos koncentration", fontsize=12)
+plt.title("Fettreserver")
+
+
+# Sparar figur i plot constrains, glukos i muskeln
+# Write the result to file
+path_result_dir = "optimering/Bilder/plot_week16_open_loop_model"
+# Check if directory exists
+if not os.path.isdir(path_result_dir):
+    os.mkdir(path_result_dir)  # Create a new directory if not existing
+path_fig = path_result_dir + "/plot_Fettreserver.jpg"
 print("path_fig = {}".format(path_fig))
 plt.savefig(path_fig)
